221f2b991df0b20267a16de9b67cef426100e644
William A. O. Brown <will.b611@gmail.com>
2018-03-09 22:59:47 +0100

    Initial commit with spring boot application with logback, lombok

---
+++ src/main/java/com/github/willb611/hellospringboot/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.github.willb611.hellospringboot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@RestController
+@Slf4j
+public class HelloWorldController {
+
+  @RequestMapping(value = "/hello", method = RequestMethod.GET)
+  public String hello() {
+    log.info("hello");
+    return "Hello world!";
+  }
+
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public void rootPath(HttpServletResponse response) throws IOException {
+    response.sendRedirect("/hello");
+  }
+}
