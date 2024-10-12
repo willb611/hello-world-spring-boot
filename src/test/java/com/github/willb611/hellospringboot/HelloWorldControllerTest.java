ef93af9f0261a844d8f1c84e6e5c990f840f0401
William A. O. Brown <will.b611@gmail.com>
2024-10-12 17:51:39 +0100

    Fix version issues, it runs again

--- src/test/java/com/github/willb611/hellospringboot/HelloWorldControllerTest.java
+++ src/test/java/com/github/willb611/hellospringboot/HelloWorldControllerTest.java
@@ -1,20 +1,19 @@
 package com.github.willb611.hellospringboot;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class HelloWorldControllerTest {
@@ -31,10 +30,10 @@ public class HelloWorldControllerTest {
 
   @Test
   public void helloEndPointShouldSayHello() throws Exception {
-    MvcResult result = mockMvc.perform(get("/hello"))
+    var result = mockMvc.perform(get("/hello"))
         .andExpect(status().is2xxSuccessful())
         .andReturn();
-    String content = result.getResponse().getContentAsString();
+    var content = result.getResponse().getContentAsString();
     assertThat(content).contains("Hello");
   }
 }
