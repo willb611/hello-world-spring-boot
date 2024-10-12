2d6d99bc30220cf3b53ec3aacbdd364684f6fd15
William A. O. Brown <will.b611@gmail.com>
2024-10-12 17:53:45 +0100

    update to spring boot 3, add mvn wrapper

--- src/main/java/com/github/willb611/hellospringboot/HelloWorldController.java
+++ src/main/java/com/github/willb611/hellospringboot/HelloWorldController.java
@@ -1,11 +1,11 @@
 package com.github.willb611.hellospringboot;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
